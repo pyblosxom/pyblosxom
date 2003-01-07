104e06c0bacac504306e988ab4e3eb8bd9867b97
Wari Wahab <pyblosxom@wari.per.sg>
2003-01-07 09:32:36 +0000

    Files removed from distribution because they are no longer relavant Removed Files: libs/plugins/pyblosxom_comments.py libs/plugins/trackback.py

--- libs/plugins/trackback.py
+++
@@ -1,9 +0,0 @@
-import cgi, xmlrpc
-
-class TrackBack:
-	def __init__(self,properties):
-		self.data = properties
-		self.form = cgi.FieldStorage()
-
-	def process(self):
-		print "Content-type: text/plain\n\nHello world";
