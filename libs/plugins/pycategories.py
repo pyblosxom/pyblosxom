b5b88e602aa43a41f02a4f6d13d465fe71e54ad2
Will Guaraldi <willg@bluesock.org>
2003-01-23 16:59:47 +0000

    Adjusted pycategories a bit so the output is no longer like this:
    dev
    dev/lyntin
    dev/pyblosxom
    dev/stringbean
    gentoo
    life
    
    and more like this:
    
    dev/
      lyntin/
      pyblosxom/
      stringbean/
    gentoo/
    life/
    
    It still needs some work.  I may introduce different ways of formatting
    the output.  I also want to count the number of entries in each category,
    but that's going to take a bit more futzing around to be able to do.

--- libs/plugins/pycategories.py
+++ libs/plugins/pycategories.py
@@ -19,13 +19,20 @@ class PyblCategories:
 			self.genCategories()
 		return self._categories
 
+	def genitem(self, item):
+		itemlist = item.split("/")
+		return (((len(itemlist)-1) * "&nbsp;&nbsp;") + 
+				"<a href=\"%s/%s\">%s</a>" % (self._baseurl, item, itemlist[-1] +"/"))
+
 	def genCategories(self):
 		root = self._py["datadir"]
-		baseurl = self._py.get("base_url", "")
+		self._baseurl = self._py.get("base_url", "")
 		clist = tools.Walk(root, pattern=None, return_folders=1)
 		clist = [mem[len(root)+1:] for mem in clist]
-		clist = ["<a href=\"%s/%s\">%s</a>" % (baseurl, mem, mem) for mem in clist]
+
 		clist.sort()
+		clist = map(self.genitem, clist)
+		# clist = ["<a href=\"%s/%s\">%s</a>" % (baseurl, mem, mem) for mem in clist]
 		self._categories = "<br>".join(clist)
 
 def load(py, entryList):
