4b6d6ff7916fae0afb417434c8a053c8085f26a5
Wari Wahab <pyblosxom@wari.per.sg>
2003-02-06 14:07:48 +0000

    Added configurable flavour targets to archive links and some __doc__

--- contrib/plugins/pyarchives.py
+++ contrib/plugins/pyarchives.py
@@ -1,4 +1,24 @@
 # vim: tabstop=4 shiftwidth=4
+"""
+Walks through your blog root figuring out all the available monthly archives in
+your blogs.  It generates html with this information and stores it in the
+$archivelinks variable which you can use in your head or foot templates.
+
+Additionally, you can specify the flavour for the link by creating an entry in
+the config.py file or the ini file with the name "archive_flavour" and the
+value of the flavour you want to use.
+
+ini-file example (in the pyblosxom section):
+
+   archive_flavour = index
+
+config.py example:
+
+   py["archive_flavour"] = "index"
+"""
+__author__ = "Wari Wahab - wari at wari dot per dot sg"
+__version__ = "$Id$"
+
 from libs import tools
 import time, os
 
@@ -15,6 +35,12 @@ class PyblArchives:
 	def genLinearArchive(self):
 		root = self._py["datadir"]
 		baseurl = self._py.get("base_url", "")
+		
+		if self._py.get("archive_flavour", ""):
+			self._flavour = "?flav=" + self._py["archive_flavour"]
+		else:
+			self._flavour = ""
+			
 		archives = {}
 		archiveList = tools.Walk(root)
 		for file in archiveList:
@@ -25,8 +51,8 @@ class PyblArchives:
 			da = time.strftime('%d',timetuple)
 			yr = time.strftime('%Y',timetuple)
 			if not archives.has_key(yr + mo_num):
-				archives[yr + mo_num] = ('<a href="%s/%s/%s">%s-%s</a><br />' % 
-										(baseurl, yr, mo, yr, mo))
+				archives[yr + mo_num] = ('<a href="%s/%s/%s%s">%s-%s</a><br />' % 
+										(baseurl, yr, mo, self._flavour, yr, mo))
 		arcKeys = archives.keys()
 		arcKeys.sort()
 		arcKeys.reverse()
