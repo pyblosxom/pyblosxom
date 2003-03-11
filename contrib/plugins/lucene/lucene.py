b446691549df143a399d3f754918531d3454b969
Ted Leung <twl@users.sourceforge.net>
2003-03-11 01:18:59 +0000

    adapt to new architecture

--- contrib/plugins/lucene/lucene.py
+++ contrib/plugins/lucene/lucene.py
@@ -11,6 +11,9 @@ __version__ = "$Id$"
 
 import glob, os, urllib
 from libs import api
+from libs.entries import fileentry
+
+# TODO - move these to config
 
 # location of Java
 JAVA_HOME = '/usr/bin/java'
@@ -23,26 +26,54 @@ classpath = ':'.join(glob.glob(lucene)+[bindir])
 # location of lucene index file
 index = '/home/twl/pyblog/index'
 
+def makeEntry(filename,config):
+    """
+    @param filename: filename of matching entry
+    @type  filename: string
+        
+    @param config: a pyblosxom config dict
+    @type  config: a dict
+    """
+    return fileentry.FileEntry(config, filename, config['datadir'])                                              
 def search(config, term):
+    """
+    Search for the specified search term
+    
+    @param config: a pyblosxom config dict
+    @type config:  a dict
+        
+    @param term: the search term
+    @type term: a string
+    """
     urllib.quote(term)
     results = os.popen(JAVA_HOME+' -cp '+classpath+' LuceneSearch '+index+' '+term,'r').readlines()
-    results = [ os.path.join(config['root_datadir'], x[2:-1]) for x in results ]
+    results = [ os.path.join(config['datadir'], x[2:-1]) for x in results ]
     f = file("/tmp/lucene","w")
     for x in results:
         f.write(x)
     f.close()
-    return results
+    return [ makeEntry(x, config) for x in results]
 
 def searchHandler(request):
-    # Lucene search handling
-    config = request.getConfiguration()
-    data = request.getData()
+    """
+    Lucene search handling
     
+    @param request: the Pyblosxom request
+    @type request: a Pyblosxom request object
+    """
+    # do nothing if the form is not a lucene form
     form = request.getHttp()['form']
     if not form.has_key("q"):
         return None
-    data['luceneResults'] = search(data, form["q"].value)
+  
+    config = request.getConfiguration()
+    data = request.getData()
+    
+    data['luceneResults'] = search(config, form["q"].value)
     return data['luceneResults']
 
 def initialize():
+    """
+    Call back chain initialization
+    """
     api.fileListHandler.register(searchHandler,api.FIRST)
