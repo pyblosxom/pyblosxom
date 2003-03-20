b42afe94e0ff6c2973afc02e4b6cd289343d6ff5
Wari Wahab <pyblosxom@wari.per.sg>
2003-03-20 10:27:44 +0000

    Fixed error that's alerted to me by Ted.

--- libs/pyblosxom.py
+++ libs/pyblosxom.py
@@ -163,7 +163,7 @@ class PyBlosxom:
                 if path_info and re.match("^([0-2][0-9]|3[0-1])$", path_info[0]):
                     # Potential day here, no more processing from here
                     data['pi_da'] = path_info.pop(0)
-            if path_info[0]:
+            if path_info and path_info[0]:
                 # Potential flavour after date
                 filename, ext = os.path.splitext(path_info[0])
                 if filename == 'index':
