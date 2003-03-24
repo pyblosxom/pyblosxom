5a371ffac63e9c6c4739b4d87c3136a34e67571c
Will Guaraldi <willg@bluesock.org>
2003-03-24 23:07:06 +0000

    Adjusted our load_plugins behavior so that if the load_plugins key is not in the config dict, then we load all the plugins in the libs/plugins directory in alphanumeric order by filename.

--- libs/plugins/__init__.py
+++ libs/plugins/__init__.py
@@ -6,10 +6,32 @@ plugins = []
 def initialize_plugins(configdict):
     """
     Imports and initializes plugins from this directory so they can register
-    with the api callbacks.  The import list is based on the "load_plugins"
-    entry in the config dict.
+    with the api callbacks.
+
+    If the user specifies a "load_plugins" list of plugins to load, then
+    we explicitly load those plugins in the order they're listed.  If the
+    load_plugins key does not exist, then we load all the plugins in the
+    plugins directory using an alphanumeric sorting order.
     """
-    plugin_list = configdict.get("load_plugins", ())
+    plugin_list = configdict.get("load_plugins", None)
+
+    # if there's no load_plugins key, then we grab all the plugins
+    # in the directory and sort them alphanumerically
+    if plugin_list == None:
+        index = __file__.rfind(os.sep)
+        if index == -1:
+            path = "." + os.sep
+        else:
+            path = __file__[:index]
+
+        plugin_list = glob.glob( os.path.join(path, "*.py"))
+
+        # remove plugins that start with a _
+        plugin_list = [p for p in plugin_list if p[0] != "_"]
+
+        plugin_list = [p[p.rfind(os.sep)+1:p.rfind(".")] for p in plugin_list]
+        plugin_list.sort()
+
 
     for mem in plugin_list:
 
