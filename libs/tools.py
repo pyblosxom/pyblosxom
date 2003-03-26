58f2797dcbc12e928ebac84efbec369fd8a49f74
Wari Wahab <pyblosxom@wari.per.sg>
2003-03-26 11:21:13 +0000

    Updated documentation to be epydoc compliant

--- libs/tools.py
+++ libs/tools.py
@@ -1,3 +1,13 @@
+# vim: tabstop=4 shiftwidth=4 expandtab
+"""
+Tools module
+
+The swiss army knife for all things pyblosxom
+
+@var month2num: A dict of literal months to its number format
+@var num2month: A dict of number month format to its literal format
+@var MONTHS: A list of valid literal and numeral months
+"""
 import sgmllib, re, os, string,  types
 import api
 
@@ -26,11 +36,15 @@ for k,v in month2num.items():
 MONTHS = num2month.keys() + month2num.keys()
 
 class Stripper(sgmllib.SGMLParser):
-    """Strips HTML"""
+    """
+    Strips HTML
+    
+    An C{SGMLParser} subclass to strip away HTMLs
+    """
     def __init__(self):
         self.data = []
         sgmllib.SGMLParser.__init__(self)
-    def unknown_starttag(self, tag, attrib): self.data.append(" ")
+    def unknown_starttag(self, tag, attrs): self.data.append(" ")
     def unknown_endtag(self, tag): self.data.append(" ")
     def handle_data(self, data): self.data.append(data)
     def gettext(self):
@@ -39,17 +53,34 @@ class Stripper(sgmllib.SGMLParser):
         return text # non - normalized whitespace
     
 class Replacer:
-    """This class is a utility class used to provide a bound method to the
-    re.sub() function. Gotten from OPAGCGI"""
+    """
+    Class for replacing variables in a template
+
+    This class is a utility class used to provide a bound method to the
+    C{re.sub()} function. Gotten from OPAGCGI
+    """
     def __init__(self, dict):
-        """The constructor. It's only duty is to populate itself with the
-        replacement dictionary passed."""
+        """
+        It's only duty is to populate itself with the replacement dictionary
+        passed.
+
+        @param dict: The dict for variable substitution
+        @type dict: dict
+        """
         self.dict = dict
 
     def replace(self, matchobj):
-        """The replacement method. This is passed a match object by re.sub(),
-        which it uses to index the replacement dictionary and find the
-        replacement string."""
+        """
+        The replacement method. 
+        
+        This is passed a match object by re.sub(), which it uses to index the
+        replacement dictionary and find the replacement string.
+
+        @param matchobj: A C{re} object containing substitutions
+        @type matchobj: C{re}
+        @returns: Substitutions
+        @rtype: string
+        """
         key = matchobj.group(1)
         if self.dict.has_key(key):
             r = self.dict[key]
@@ -63,9 +94,20 @@ class Replacer:
             return u''
 
 def parse(dict, template):
-    """parse(dict) -> string
+    """
+    parse(dict) -> string
+    
     This method parses the open file object passed, replacing any keys
-    found using the replacement dictionary passed. From OPAGCGI library"""
+    found using the replacement dictionary passed. Uses the Replacer object.
+    From OPAGCGI library
+
+    @param dict: The name value pair list containing variable replacements
+    @type dict: dict
+    @param template: A template file with placeholders for variable replacements
+    @type template: string
+    @returns: Substituted template
+    @rtype: string
+    """
     replacer = Replacer(dict).replace
     replaced = u'' + re.sub(ur'(?<!\\)\$([A-Za-z0-9_\-]+)', replacer, template)
     return replaced
@@ -76,6 +118,9 @@ def filestat(filename):
     stats on a given file are.  
 
     This calls the filestat chain and returns the final mtime.
+
+    @param filename: The file to get the time from
+    @type filename: string
     """
     args = { "filename": filename, "mtime": os.stat(filename) }
     return api.filestat.executeTransform(args)["mtime"]
@@ -84,8 +129,13 @@ def logRequest(filename = '', return_code = '200'):
     """
     Calls the api's logRequest callback chain to do some statistical analysis
     based on the current request.
-
+    
     This calls the chain with (filename, returnCode) and returns None
+
+    @param filename: Base filename to log to
+    @type filename: string
+    @param return_code: HTTP standard return code
+    @type return_code: string
     """
     args = {"filename": filename, "return_code": return_code }
     api.logRequest.executeHandler(args)
@@ -93,6 +143,9 @@ def logRequest(filename = '', return_code = '200'):
 def fileList(request):
     """
     Takes an entry dict and returns a file list
+
+    @param request: A standard Request object
+    @type request: C{libs.Request.Request}
     """
     return api.fileListHandler.executeListHandler({"request": request})
 
@@ -105,6 +158,20 @@ def Walk(root = '.',
     and returns a list of all of the files (and optionally folders) that match
     our pattern(s). Taken from the online Python Cookbook and modified to own
     needs
+
+    @param root: Starting point to walk from
+    @type root: string
+    @param recurse: Depth of recursion,
+        - 0: All the way
+        - 1: Just this level
+        - I{n}: I{n} depth of recursion
+    @type recurse: integer
+    @param pattern: A C{re.compile}'d object
+    @type pattern: object
+    @param return_folders: If true, just return list of folders
+    @type return_folders: boolean
+    @returns: A list of file paths
+    @rtype: list
     """
     # initialize
     result = []
@@ -145,7 +212,18 @@ def Walk(root = '.',
     return result
 
 def importName(modulename, name):
-    """Module importer, for modules that can only be determined during runtime"""
+    """
+    Module importer
+    
+    For modules that can only be determined during runtime
+
+    @param modulename: The base name of the module to import from
+    @type modulename: string
+    @param name: The name of the module to import from the modulename
+    @type name: string
+    @returns: If successful, returns an imported object reference, else C{None}
+    @rtype: object
+    """
     try:
         module = __import__(modulename, globals(), locals(), [name])
     except ImportError:
@@ -157,6 +235,18 @@ def importName(modulename, name):
 
 
 def sortDictBy(list, key):
+    """
+    Sort dict by a key
+
+    Sorts a list of dicts with a specific key in the dict
+
+    @param list: A list of dicts
+    @type list: list
+    @param key: The key in the list to sort with
+    @type key: string
+    @returns: A new list with sorted entries
+    @rtype: list
+    """
     nlist = map(lambda x, key=key: (x[key], x), list)
     nlist.sort()
     nlist.reverse()
@@ -164,6 +254,19 @@ def sortDictBy(list, key):
 
 
 def generateRandStr(minlen=5, maxlen=10):
+    """
+    Generate a random string
+    
+    Tool to generate a random string between C{minlen} to C{maxlen} characters
+
+    @param minlen: The minimum length the string should be
+    @type minlen: integer
+    @param maxlen: The maximum length the string could be
+    @type maxlen: integer
+
+    @returns: A string containing random characters
+    @rtype: string
+    """
     import whrandom
     chars = string.letters + string.digits
     randStr = ""
@@ -189,6 +292,12 @@ def get_registry():
     return my_registry
 
 def get_cache():
+    """
+    Pads the global registry dict with a cache object
+
+    @returns: A BlosxomCache object reference
+    @rtype: C{libs.cache.base.BlosxomCacheBase} subclass
+    """
     registry = get_registry()
 
     mycache = registry.get("cache", "")
