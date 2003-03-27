bd9170b50a84f7f3f13f067e4eeb00cd311bdadb
Wari Wahab <pyblosxom@wari.per.sg>
2003-03-27 23:52:07 +0000

    Preformatters now use easier to understand plugin API and callback mechanism

--- libs/preformatters/plain.py
+++
@@ -1,7 +0,0 @@
-"""Interface for preformatters
-"""
-# Plain preformatter
-# This one does nothing except to join strings
-from libs.preformatters.base import PreFormatterBase
-class PreFormatter(PreFormatterBase):
-    pass
