5b02dff782c184937bb6ac4fff421f443e640176
Wari Wahab <pyblosxom@wari.per.sg>
2003-04-23 05:23:26 +0000

    Added Lee Joramo's patch for mt2blosxom to support Radio's MT Export. Also in the patch, some useful metadata stored in the content as well.

--- contrib/mt2blosxom.py
+++ contrib/mt2blosxom.py
@@ -8,17 +8,37 @@ import random
 
 entryDelim = re.compile( r"^--------$", re.MULTILINE )
 sectionDelim = re.compile( r"^-----$", re.MULTILINE )
-    
-def convert(inputfile, outputdir):    
+
+print "start"
+def convert(inputfile, output_dir_root):    
     input = open(inputfile).read()
     entries = entryDelim.split(input)
+    print "start"
     for entry in entries:
         if entry.strip():
             sections = sectionDelim.split(entry)
             body = sections[1].split(":",1)[1]
-            extendedbody = sections[2].split(":",1)[1]
+            #lee.list@joramo.com 
+            #2003-02-12 
+            #    check to see if extended sections exist
+            #    when exporting from Radio Userland to Moveable Typle
+            #    no extendedbody is created
+            extendedbody = ""
+            if len(sections) > 2:
+                print sections
+                extendedbody = sections[2].split(":",1)[1]
             fields = sections[0].strip().split("\n")
             publish = 0
+            body = sections[1].split(":",1)[1]
+            #lee.list@joramo.com 
+            #2003-02-12 
+            #     Radio Userland -> MT export
+            #     may not include all fields, so need to to preset
+            title = "<!-- No Title -->"
+            date = "01/01/2000 00:01:00 AM"
+            category = ""
+            author = ""
+            publish = 1
             for field in fields:
                 if field.strip():
                     tag, value = field.split(": ", 1)
@@ -26,14 +46,40 @@ def convert(inputfile, outputdir):
                         title = value.strip()
                     elif tag == "DATE":
                         date = value.strip()
+                    elif tag == "CATEGORY":
+                        category = value.strip()
+                    elif tag == "AUTHOR":
+                        author = value.strip()
                     elif tag == "STATUS":
+                        publish = 0
                         if value.strip() == "publish":
                             publish = 1
+            #lee.list@joramo.com
+            #2003-02-12
+            #try to a variety of time formats
+            timeformats = ("%m/%d/%Y %H:%M:%S %p", "%m/%d/%y %H:%M:%S %p")
+            for timeformat in timeformats:
+                formatmatched = 0
+                try:
+                    timestamp = time.mktime(time.strptime(date, timeformat))
+                    formatmatched = 1
+                except:
+                    continue
+                if formatmatched == 1:
+                    break
+            if formatmatched == 0:
+                raise ValueError, "timeformat not matched\n"+date
 
-            timestamp = time.mktime(time.strptime(date, "%m/%d/%Y %H:%M:%S %p"))
+            output_directory = output_dir_root+"/"+category
+            if not os.path.exists(output_directory):
+                os.mkdir(output_directory, 0755)
+            
             outputfile = title.strip()
             if not outputfile: outputfile = body[:20].strip()
-            outputfile= "%s/%s" % (outputdir, re.sub(r"[^a-zA-Z0-9]", "_", outputfile))
+
+            outputfile= "%s/%s" % (output_directory, re.sub(r"[^a-zA-Z0-9]", "_", outputfile))
+            
+
             if os.path.isfile(outputfile + ".txt") or os.path.isfile(outputfile + ".txt-"):
                 outputfile = outputfile + str(random.random()).split(".")[1]
             if publish:
@@ -43,6 +89,9 @@ def convert(inputfile, outputdir):
 
             output = open(outputfile, "w")
             output.write(title+"\n")
+            output.write("<!--timestamp:%s:-->\n"%timestamp)
+            output.write("<!--category:%s:-->\n"%category)
+            output.write("<!--author:%s:-->\n"%author)
             output.write(body)
             output.write(extendedbody)
             output.close()
@@ -50,12 +99,12 @@ def convert(inputfile, outputdir):
             os.utime(outputfile, (timestamp, timestamp)) 
 if __name__=="__main__":
     try:
-        inputfile, outputdir = sys.argv[1:]
+        inputfile, output_dir_root = sys.argv[1:]
     except:
         print """Usage: mt2blosxom mtexportfile datadir
         mtexportfile - File containing exported Movable Type entries
         datadir -  Blosxom directory to put files in"""
-	sys.exit(0)
+        sys.exit(0)
 
-    convert(inputfile, outputdir)
+    convert(inputfile, output_dir_root)
 
