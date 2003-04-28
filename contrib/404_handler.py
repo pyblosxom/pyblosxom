fcb5dafc9f2a61bc0a9afeb4b5eeaa7587ee3421
Wari Wahab <pyblosxom@wari.per.sg>
2003-04-28 07:58:39 +0000

    Updated 404 fried to baked pages handler to support the new pyblosxom calls Features added: 	* If error is 404, don't create a file 	* Supports months, years, categories (may be buggy) 	* Supports entries that are not .txt files TODO: 	* Fix categories and months file creation to do an index.html file 	  instead of it being a file.

--- contrib/404_handler.py
+++ contrib/404_handler.py
@@ -2,34 +2,34 @@
 # vim: shiftwidth=4 tabstop=4 expandtab
 """An error 404 page handler
 This is a helper for pyblosxom, see:
-http://roughingit.subtlehints.net/py_stuff/weblogs/hacks/Half_Baked_and_a_Mostly_Fried.html
-for details
+    http://roughingit.subtlehints.net/py_stuff/weblogs/hacks/Half_Baked_and_a_Mostly_Fried.html
+    for details
 """
+# Uncomment this to look for libs/ directory elsewhere
+#sys.path.append('/path/to/pyblosxom/installation/dir')
 # Uncomment to debug (May not work due to buffering)
 #import cgitb; cgitb.enable()
 import os, cgi, sys, traceback, re
 import config
-# Uncomment this to look for libs/ directory elsewhere
-#sys.path.append('/path/to/libs/directory')
 
-# Essential data stuff
-archiveRoot = '/py_stuff'
-docRoot = 'py_stuff'
-pyblosxomScriptPath = '/pyblosxom'
-hostName = 'roughingit.subtlehints.net'
+# Essential data stuff - Edit this for site specific information
+archiveRoot = '/myblog'
+docRoot = 'myblog'
+pyblosxomScriptPath = '/cgi-bin/pyblosxom.cgi'
+hostName = 'www.example.com'
 
 py = config.py
-xmlrpc = config.xmlrpc
 
 # ------------ Nothing to change here --------------- #
 __author__ = 'Wari Wahab <wari@wari.per.sg>'
-__version__ = "0+5i_rev1"
+__version__ = config.__version__
 __date__ = "$Date$"
 __copyright__ = "Copyright (c) 2002 Wari Wahab"
 __license__ = "Python"
 py['pyblosxom_version'] = __version__
 py['pyblosxom_name'] = 'pyblosxom'
 
+x
 from StringIO import StringIO
 from libs.pyblosxom import PyBlosxom
 
@@ -48,21 +48,39 @@ def saveData(filename, document):
     print document
     # Now we disect the document and remove the first few lines
     data = document.split('\n')
-    file(filename, "w").write('\n'.join(data[4:]))
+    line = data.pop(0)
+    while line != '':
+        if line == 'Status: 404 Not Found':
+            return
+        line = data.pop(0)
+    file(filename, "w").write('\n'.join(data))
 
 if __name__ == '__main__':
     redir = os.environ.get('REDIRECT_URI', '')
     if redir == '':
         redir = os.environ.get('REDIRECT_URL', '')
     pathInfo = re.sub(archiveRoot, '', redir)
-    realfile = py['datadir'] + re.sub('.html', '.txt', pathInfo)
-    if re.match(archiveRoot, redir) and os.path.exists(realfile):
+    if redir.startswith(archiveRoot):
         stdout = sys.stdout
         sys.stdout = StringIO()
         os.environ['PATH_INFO'] = pathInfo
         os.environ['SCRIPT_NAME'] = pyblosxomScriptPath
         os.environ['HTTP_HOST'] = hostName
-        p = PyBlosxom(py, xmlrpc)
+        from libs.pyblosxom import PyBlosxom
+        from libs.Request import Request
+        import os, cgi
+
+        req = Request()
+        req.addConfiguration(config.py)
+
+        d = {}
+        for mem in ["PATH_INFO", "SCRIPT_NAME", "REQUEST_METHOD", "HTTP_HOST", "QUERY_STRING", "REQUEST_URI", "HTTP_USER_AGENT", "REMOTE_ADDR"]:
+            d[mem] = os.environ.get(mem, "")
+        req.addHttp(d)
+
+        req.addHttp({"form": cgi.FieldStorage()})
+
+        p = PyBlosxom(req)
         p.startup()
         p.run()
         data = sys.stdout.getvalue()
@@ -71,5 +89,5 @@ if __name__ == '__main__':
         sys.exit()
         
     else:
-        print "Content-Type: text/html\n"
+        print "Error: 404\nContent-Type: text/html\n"
         print "Error: Page not found, proceed to <a href=/>RoughingIT</a><br />\n"
