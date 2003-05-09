2536188f2724ff82f921db7f05476c682f55411b
Wari Wahab <pyblosxom@wari.per.sg>
2003-05-09 15:49:27 +0000

    Remove pat and pat_list again :) This time I removed the break as well
    The reason why it didn't work previously is due to me forgetting to remove the
    break in the previous for loop.. pat requires pattern to be a string, but
    pattern is always a re object. so, the result of pat_list is always a [';'].
    It didn't make sense at all to do an iteration with one value in a list and the
    value is not used at all.
    
    pat and pat_list was a legacy tools.Walk thingy, see:
    http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/52664 for the real
    usage of pat and pat_list.

--- libs/tools.py
+++ libs/tools.py
@@ -191,19 +191,14 @@ def Walk(root = '.', recurse = 0, pattern = '', return_folders = 0 ):
         ext = registry["request"].getData()['extensions']
         pattern = re.compile(r'.*\.(' + '|'.join(ext) + r')$')
 
-    #pattern = pattern or re.compile('.*\.txt$')
-    pat_list = string.splitfields( pattern, ";" )
-    
     # check each file
     for name in names:
         fullname = os.path.normpath(os.path.join(root, name))
 
         # grab if it matches our pattern and entry type
-        for pat in pat_list:
-            if pattern.match(name):
-                if (os.path.isfile(fullname) and not return_folders) or (return_folders and os.path.isdir(fullname)):
-                    result.append(fullname)
-                break
+        if pattern.match(name):
+            if (os.path.isfile(fullname) and not return_folders) or (return_folders and os.path.isdir(fullname)):
+                result.append(fullname)
                 
         # recursively scan other folders, appending results
         if (recurse == 0) or (recurse > 1):
