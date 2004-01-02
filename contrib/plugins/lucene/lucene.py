9c387f45e43589a6bb24dc72d17e0f457ca5f496
Ted Leung <twl@users.sourceforge.net>
2004-01-02 01:44:38 +0000

    Added changes due to James Jones <jcpirate@cox.net> Also separate popen call to aid in debugging

--- contrib/plugins/lucene/lucene.py
+++ contrib/plugins/lucene/lucene.py
@@ -20,13 +20,19 @@ To install:
   <form id="searchform" method="get" action="/blog">
    <table>
     <tr>
-     <td><input type="text" id="q" name="q" size="18" maxlength="255" value="" /></td>
+     <td><input type="text" id="search" name="search" size="18" maxlength="255" value="" /></td>
      <td><input type="submit" value=Search /></td>
     </tr>
    </table>
   </form>
 
-  The action of the form should be the top level URI of your blog
+   The action of the form should be the top level URI of your blog
+
+9) You should add $searchHeader somewhere in the header of your webpage; this
+   is where statements like, "Your search returned X results for Y" are
+   placed. This statement is enclosed in a div tag with a class of 
+   "searchtext" so that you can define it as you like in your stylesheet.
+
 """
 
 __author__ = "Ted Leung - twl@sauria.com"
@@ -58,13 +64,37 @@ def search(config, term):
     JAVA_HOME=config['JAVA_HOME']
     classpath = ':'.join(glob.glob(config['lucene_home']+'/*.jar')+[config['lucene_bin']])
     index = config['lucene_index']
-    results = os.popen(JAVA_HOME+' -cp '+classpath+' LuceneSearch '+index+' '+term,'r').readlines()
+    cmd =JAVA_HOME+' -cp '+classpath+' LuceneSearch '+index+' '+term
+    pipe = os.popen(cmd,'r')
+    results = pipe.readlines()
+    status = pipe.close()
     results = [ os.path.join(config['datadir'], x[2:-1]) for x in results ]
     entries = [ makeEntry(x, config) for x in results]
     entries = [ ( x._mtime, x ) for x in entries ]
     entries.sort()
     entries.reverse()
     return [ x[1] for x in entries ]
+
+def cb_prepare(args):
+    """
+    Add a nice header for the Lucene search, this header
+    goes into the $searchHeader variable for including in
+    the header template file.
+    """
+    # do nothing if the form is not a lucene form
+    request = args["request"]
+    form = request.getHttp()['form']
+    if not form.has_key("search"):
+        return None
+                                                                                
+    data = request.getData()
+
+    resultnumber = len(data['luceneResults'])
+
+    if resultnumber < 1:
+	data['searchHeader'] = "<div class=\"searchtext\">Your search returned no results. Maybe I just never talk about <b>" + form["search"].value + "</b>. Try again.</div><div>"
+    else:
+	data['searchHeader'] = "<div class=\"searchtext\">Your search returned " + str(resultnumber) + " results for <b>" + form["search"].value + "</b>. They are below:</div>"
 
 def cb_filelist(args):
     """
@@ -76,12 +106,11 @@ def cb_filelist(args):
     # do nothing if the form is not a lucene form
     request = args["request"]
     form = request.getHttp()['form']
-    if not form.has_key("q"):
+    if not form.has_key("search"):
         return None
   
     config = request.getConfiguration()
     data = request.getData()
     
-    data['luceneResults'] = search(config, form["q"].value)
+    data['luceneResults'] = search(config, form["search"].value)
     return data['luceneResults']
-
