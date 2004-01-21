4f94ac78b872c7d7832191938990ffa07665668c
Will Guaraldi <willg@bluesock.org>
2004-01-21 18:55:47 +0000

    I made a lot of changes to the logstats plugin adding a "detailed" page for referrers listing.  This doesn't really change the basic things the logstats plugin does, but it does lay the groundwork for the ability to do a detailed report on the data.

--- contrib/plugins/logstats.py
+++ contrib/plugins/logstats.py
@@ -12,14 +12,22 @@ You can control the number of referrers displayed in config.py::
 You can control the length of the referrer string in config.py::
     py['referrer_length'] = l
 
+You can prevent some referrers from showing up in the list with the
+refer_blacklist property in config.py:
+
+    py['refer_blacklist'] = "bluesock.org,sauria.com"
+
 TODO:
 Compute some additional statistics
+
+(1/10/2004 - Heavy edits by Will)
 """
 
 __author__ = "Ted Leung - twl@sauria.com"
 __version__ = "$Id$"
 
 import os, re, string, time, cPickle
+from Pyblosxom import tools, entries
 
 class PyblStats:
     def __init__(self, config):
@@ -80,13 +88,13 @@ class PyblStats:
             uri = tuple[0]
             count = tuple[1]
             size = self._referrer_length
-            vis = string.replace(uri,'http://','')
+            vis = uri
             if len(bad_list) > 0:
                 for pat in bad_list:
-                    if re.search(pat, uri):
+                    if uri == "" or re.search(pat, uri):
                         return ""
             if len(uri) > size: vis = vis[:size]+'...'
-            return ("""<a href="%(uri)s" title="%(uri)s">%(vis)s (%(count)d)</a><br />\n""" 
+            return ("""<a href="%(uri)s" title="%(uri)s">%(vis)s</a> (%(count)d)<br />\n""" 
                     % {'uri': uri, 'vis': vis, 'count': count})
 
         def compareCounts(tuple1, tuple2):
@@ -114,6 +122,97 @@ class PyblStats:
         self._referrersText = string.join(refs[0:self._num_referrers-1])
         return self._referrersText
 
+
+    def genReferrersFiltered(self):
+        """
+        Generate the list of referring files
+        """
+        # initialize blacklist
+        bad_list = self._config.get('refer_blacklist','')
+        if bad_list:
+            bad_list = string.split(bad_list,',')
+        else:
+            bad_list = []
+
+        search_list = []
+        search_list.append("www.google")
+        search_list.append("google.com")
+        search_list.append("search.yahoo.com")
+        search_list.append("search.virgilio.it")
+        search_list.append("aolsearch.aol.com")
+        search_list.append("aolsearch.aol.co.uk")
+        search_list.append("search.netscape.com")
+        search_list.append("home.excite.co")
+        search_list.append("search.earthlink.net")
+        search_list.append("dion.excite.co.jp")
+        search_list.append("altavista.com")
+        search_list.append("mysearch.com")
+        search_list.append("mysearch.myway.com")
+        search_list.append("search.aol.com")
+        search_list.append("search.msn.com")
+        search_list.append("mail.yahoo.com")
+
+        def url(tuple):
+            """
+            Markup (and truncate) a referrer URL
+            """
+            uri = tuple[0]
+            count = tuple[1]
+            size = self._referrer_length
+            vis = uri
+
+            if len(bad_list) > 0:
+                for pat in bad_list:
+                    if uri == "" or re.search(pat, uri):
+                        return ""
+
+            for pat in search_list:
+               if uri == "" or re.search(pat, uri):
+                  return ""
+
+            if len(uri) > size: vis = vis[:size]+'...'
+            return ("""<a href="%(uri)s" title="%(uri)s">%(vis)s</a> (%(count)d)<br />\n""" 
+                    % {'uri': uri, 'vis': vis, 'count': count})
+
+        def compareCounts(tuple1, tuple2):
+            """
+            Compare tuple1 and tuple2 by their second element
+            """
+            count1 = tuple1[1]
+            count2 = tuple2[1]
+            if count1 > count2:
+                return -1 # reverse order
+            if count1 < count2:
+                return 1
+            return 0
+
+        items = self._referrers.items()
+        # sort in alphabetical order first
+        items.sort()
+        # sort list by number of occurances
+        items.sort(compareCounts)
+        # make a list of urls
+        refs = [ url(x) for x in items ]
+        # remove blanks
+        refs = [ x for x in refs if x != "" ]
+
+        self._referrersText = string.join(refs[0:self._num_referrers-1])
+        return self._referrersText
+
+
+    def genReferrerStats(self):
+        output = ["<p>"]
+        output.append("<b>Top %d referers (raw):</b><br><br>" % self._num_referrers)
+        output.append(self.genReferrers())
+        output.append("</p>")
+
+        output.append("<p>")
+        output.append("<b>Top %d referrers (filtered):</b><br><br>" % self._num_referrers)
+        output.append(self.genReferrersFiltered())
+        output.append("</p>")
+
+        return "".join(output)
+
 def cb_prepare(args):
     """
     Callback registered with prepareChain.  This does all the work
@@ -125,9 +224,10 @@ def cb_prepare(args):
     config = request.getConfiguration()
     data = request.getData()
     httpData = request.getHttp()
+    datadir = config["datadir"]
 
+    filename = datadir + '/logfile.dat'
     try:
-        filename = config['logfile']+'.dat'
         f = open(filename)
         stats = cPickle.load(f)
         stats._request = request
@@ -150,3 +250,47 @@ def cb_prepare(args):
     stats._config = None
     f = open(filename,"w")
     cPickle.dump(stats, f)
+    f.close()
+
+
+def generate_entry(output):
+    """
+    Takes a bunch of text and generates an entry out of it.  It creates
+    a timestamp so that conditionalhttp can handle it without getting
+    all fussy.
+    """
+    entry = entries.base.EntryBase()
+
+    entry['title'] = "Referrers List"
+    entry['filename'] = "referrerlist"
+    entry['file_path'] = "referrerlist"
+    entry._id = "referrerlist"
+
+    entry.setTime(time.localtime())
+
+    entry.setData(output)
+    return entry
+
+def cb_filelist(args):
+    request = args["request"]
+    pyhttp = request.getHttp()
+    config = request.getConfiguration()
+    datadir = config["datadir"]
+
+    if not pyhttp["PATH_INFO"].startswith("/referrers"):
+        return
+
+    filename = datadir + '/logfile.dat'
+    try:
+        f = open(filename)
+        stats = cPickle.load(f)
+        stats._request = request
+        stats._config = config
+        stats._referrer_length = int(config.get('referrer_length', 15))
+        stats._num_referrers = int(config.get('num_referrers', 15))
+        f.close()
+
+    except (EOFError, IOError):
+        stats = PyblStats(config)
+
+    return [generate_entry(stats.genReferrerStats())]
