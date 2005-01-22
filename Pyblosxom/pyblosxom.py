68e56cf3e5e14ffe706f94762ad006db5fe9e360
Will Guaraldi <willg@bluesock.org>
2005-01-22 14:24:06 +0000

    If the user doesn't set num_entries, we should treat it as if they set it to 0.  So I fixed the code to do that.

--- Pyblosxom/pyblosxom.py
+++ Pyblosxom/pyblosxom.py
@@ -567,7 +567,7 @@ def blosxom_file_list_handler(args):
         valid_list = entrylist
 
     # This is the maximum number of entries we can show (zero indicates show all entries)
-    if config['num_entries']:
+    if config.get('num_entries', 0):
         valid_list = valid_list[:config['num_entries']]
     valid_list = [x[1] for x in valid_list]
 
