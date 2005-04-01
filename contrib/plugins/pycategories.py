8f6f9a48076e47503e7c9f1e76573ceb46d05f35
Will Guaraldi <willg@bluesock.org>
2005-04-01 00:56:03 +0000

    Minor overhaul of pycategories to support templating in the config.py file.  It's also more sophisticated now so it can handle other ways of rendering other than indent by &nbsp; .  This will make it appeal to non-Will people.

--- contrib/plugins/pycategories.py
+++ contrib/plugins/pycategories.py
@@ -5,13 +5,39 @@ and how many entries are in each category.  It generates html with
 this information and stores it in the $categorylinks variable which
 you can use in your head or foot templates.
 
-Additionally, you can specify the flavour for the link by creating an 
-entry in the config.py file or the ini file with the name 
-"category_flavour" and the value of the flavour you want to use.
+You can format the output by setting the "category_begin", "category_item", and
+"category_end" properties.
 
-config.py example::
+Categories exist in a hierarchy.  The "category_begin" property begins a 
+category group and the "category_end" property ends a category group.  The
+"category_item" property is the template for each category item.
 
-   py["category_flavour"] = "index"
+For example, the following properties will use <ul> to open a category, </ul>
+to close a category and <li> for each item:
+
+py["category_begin"] = "<ul>"
+py["category_item"] = r'<li><a href="%(base_url)s/%(category)sindex">%(category)s</a></li>'
+py["category_end"] = "</ul>"
+
+
+Another example, the following properties don't have a begin or an end but
+instead use indentation for links and displays the number of entries in that
+category:
+
+py["category_begin"] = ""
+py["category_item"] = r'%(indent)s<a href="%(base_url)s/%(category)sindex">%(category)s</a> (%(count)d)<br />'
+py["category_end"] = ""
+
+There are no variables available in the category_begin or category_end templates.
+
+Available variables in the category_item template:
+
+  base_url      (this is set in your config.py file)   string
+  fullcategory  'dev/pyblosxom/status/'                string
+  category      'status/'                              string
+  flavour       'html'                                 string
+  count         70                                     int
+  indent        '&nbsp;&nbsp;&nbsp;&nbsp;'             string
 """
 __author__ = "Will Guaraldi - willg at bluesock dot org"
 __version__ = "$Id$"
@@ -19,12 +45,17 @@ __version__ = "$Id$"
 from Pyblosxom import tools
 import re, os
 
+DEFAULT_BEGIN = r'<ul class="categorygroup">'
+DEFAULT_ITEM = r'<li><a href="%(base_url)s/%(fullcategory)sindex.%(flavour)s">%(category)s</a> (%(count)d)</li>'
+DEFAULT_END = "</ul>"
+
 def verify_installation(request):
     config = request.getConfiguration()
-    if not config.has_key("category_flavour"):
-        print "missing optional config property 'category_flavour' which allows "
-        print "you to specify the flavour for the category link.  refer to "
-        print "pycategory plugin documentation for more details."
+    if not config.has_key("category_template"):
+        print "missing optional config property 'category_template' which allows "
+        print "you to specify how the category hierarchy is rendered.  see"
+        print "the documentation at the top of the pycategories plugin code "
+        print "file for more details."
     return 1
 
 class PyblCategories:
@@ -38,61 +69,95 @@ class PyblCategories:
             self.genCategories()
         return self._categories
 
-    def genitem(self, item):
-        itemlist = item.split(os.sep)
-
-        num = 0
-        for key in self._elistmap.keys():
-            if key.endswith(item) or key.endswith(item + os.sep):
-                num = num + self._elistmap[key]
-        num = " (%d)" % num
-
-        if not item:
-            tab = ""
-        else:
-            tab = len(itemlist) * "&nbsp;&nbsp;"
-
-        return (tab + "<a href=\"%s/%s%s\">%s</a>%s" % (self._baseurl, item, self._flavour, itemlist[-1] +"/", num))
-
     def genCategories(self):
         config = self._request.getConfiguration()
         root = config["datadir"]
 
-        data = self._request.getData()
-
-        flav = config.get("category_flavour", "")
-        if flav:
-            self._flavour = "?flav=" + flav
-        else:
-            self._flavour = ""
+        begin_t = config.get("category_begin", DEFAULT_BEGIN)
+        item_t = config.get("category_item", DEFAULT_ITEM)
+        end_t = config.get("category_end", DEFAULT_END)
 
         self._baseurl = config.get("base_url", "")
 
-        # build the list of entries
+        form = self._request.getForm()
+        flavour = (form.has_key('flav') and form['flav'].value or 
+            config.get('default_flavour', 'html'))
+
+        # build the list of all entries in the datadir
         elist = tools.Walk(self._request, root)
+
+        # peel off the root dir from the list of entries
         elist = [mem[len(root)+1:] for mem in elist]
 
-        total = len(elist)
-
+        # go through the list of entries and build a map that
+        # maintains a count of how many entries are in each 
+        # category
         elistmap = {}
         for mem in elist:
             mem = os.path.dirname(mem)
             elistmap[mem] = 1 + elistmap.get(mem, 0)
         self._elistmap = elistmap
 
+        # go through the elistmap keys (which is the list of
+        # categories) and for each piece in the key (i.e. the key
+        # could be "dev/pyblosxom/releases" and the pieces would
+        # be "dev", "pyblosxom", and "releases") we build keys
+        # for the category list map (i.e. "dev", "dev/pyblosxom",
+        # "dev/pyblosxom/releases")
         clistmap = {}
         for mem in elistmap.keys():
             mem = mem.split(os.sep)
-            for i in range(len(mem)+1):
-                p = os.sep.join(mem[0:i])
+            for index in range(len(mem)+1):
+                p = os.sep.join(mem[0:index])
                 clistmap[p] = 0
 
+        # then we take the category list from the clistmap and
+        # sort it alphabetically
         clist = clistmap.keys()
         clist.sort()
 
-        clist = map(self.genitem, clist)
-        self._categories = "<br />".join(clist)
+        output = []
+        indent = 0
 
+        # then we generate each item in the list
+        for item in clist:
+            itemlist = item.split(os.sep)
+
+            num = 0
+            for key in self._elistmap.keys():
+                if key.endswith(item) or key.endswith(item + os.sep):
+                    num = num + self._elistmap[key]
+
+            if not item:
+                tab = ""
+            else:
+                tab = len(itemlist) * "&nbsp;&nbsp;"
+
+            if indent > len(itemlist):
+                for i in range(indent - len(itemlist)):
+                    output.append(end_t)
+
+            elif indent < len(itemlist):
+                for i in range(len(itemlist) - indent):
+                    output.append(begin_t)
+
+            # now we build the dict with the values for substitution
+            d = { "base_url":     self._baseurl, 
+                  "fullcategory": item + "/", 
+                  "category":     itemlist[-1] + "/", 
+                  "flavour":      flavour,
+                  "count":        num,
+                  "indent":       tab }
+
+            # and we toss it in the thing
+            output.append(item_t % d)
+
+            indent = len(itemlist)
+
+        output.append(end_t)
+
+        # then we join the list and that's the final string
+        self._categories = "\n".join(output)
 
 def cb_prepare(args):
     request = args["request"]
