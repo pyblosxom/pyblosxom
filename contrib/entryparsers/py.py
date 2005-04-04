f158c8d14aeea0611a5aa7e6c185388117fae8f8
Will Guaraldi <willg@bluesock.org>
2005-04-04 20:50:38 +0000

    Updated license and version information for all the plugins and scripts that Wari was the original author on.  (With Wari's permission--see pyblosxom-devel mailing list archives.)

--- contrib/entryparsers/py.py
+++ contrib/entryparsers/py.py
@@ -1,8 +1,32 @@
 """
-    MoinMoin - Python Source Parser
+MoinMoin - Python Source Parser
 
-    A more complex example of how to use an entryparser callback
+A more complex example of how to use an entryparser callback
+
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Copyright 2004, 2005 Wari Wahab
 """
+__version__ = "$Id$"
 
 def cb_entryparser(args):
     args['py'] = parse
--- contrib/xmlrpc_plugins/xmlrpc.py
+++ contrib/xmlrpc_plugins/xmlrpc.py
@@ -17,10 +17,32 @@ py['xmlrpc_maxrequest_length'] = 10000
                to handle the request.
 'maxrequest_length' - the maximum content length of a post.  this is
                partially to protect your blog from bad people.
-"""
 
-__author__ = "Wari"
-__version__ = "$id$"
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Copyright 2004, 2005 Wari Wahab
+"""
+__author__ = "Wari Wahab"
+__version__ = "$Id$"
 __description__ = "XMLRPC services dispatcher"
 
 import xmlrpclib, sys, os
