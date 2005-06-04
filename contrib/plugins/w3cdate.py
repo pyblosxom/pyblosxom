3f28a7ef54e30e735c25f43251787d165cb5b7b7
Will Guaraldi <willg@bluesock.org>
2005-06-04 15:08:16 +0000

    Added code written by Matej Cepl which removes the requirement for PyXML to be installed.

--- contrib/plugins/w3cdate.py
+++ contrib/plugins/w3cdate.py
@@ -1,17 +1,23 @@
 """
 Adds a 'w3cdate' variable to every entry which has the mtime of the entry
-in ISO8601 format
+in ISO8601 format.
 
 Adds a 'w3cdate' variable to the head and foot templates which has the mtime
 of the first entry in the entrylist being displayed (this is often the
 youngest/most-recent entry).
 
 
-WARNING: you must have PyXML installed as part of your python installation 
-in order for this plugin to work
+Note: When adding this plugin to the load_plugins list, it helps 
+to put the plugin early in the list so that the data will be 
+available to subsequent plugins.
 
-Place this plugin early in your load_plugins list, so that the w3cdate will
-be available to subsequent plugins
+Note: You might get better results if you have PyXMl installed as 
+part of your Python installation.  If you don't, then we fudge the 
+date using a home-brew function.
+
+
+Thanks to Matej Cepl for the hacked iso8601 code that doesn't require
+PyXML.
 
 
 Permission is hereby granted, free of charge, to any person obtaining
@@ -40,9 +46,44 @@ __version__ = "$Id$"
 __copyright__ = "Copyright (c) 2003-2005 Ted Leung"
 __license__ = "Python"
 
-import xml.utils.iso8601
+import rfc822
 import time
 from Pyblosxom import tools
+
+def iso8601_hack_tostring(t, timezone):
+    timezone = int(timezone)
+    if timezone:
+        sign = (timezone < 0) and "+" or "-"
+        timezone = abs(timezone)
+        hours = timezone / (60 * 60)
+        minutes = (timezone % (60 * 60)) / 60
+        tzspecifier = "%c%02d:%02d" % (sign, hours, minutes)
+    else:
+        tzspecifier = "Z"
+    psecs = t - int(t)
+    t = time.gmtime(int(t) - timezone)
+    year, month, day, hours, minutes, seconds = t[:6]
+    if seconds or psecs:
+        if psecs:
+            psecs = int(round(psecs * 100))
+            f = "%4d-%02d-%02dT%02d:%02d:%02d.%02d%s"
+            v = (year, month, day, hours, minutes, seconds, psecs, tzspecifier)
+        else:
+            f = "%4d-%02d-%02dT%02d:%02d:%02d%s"
+            v = (year, month, day, hours, minutes, seconds, tzspecifier)
+    else:
+        f = "%4d-%02d-%02dT%02d:%02d%s"
+        v = (year, month, day, hours, minutes, tzspecifier)
+    return f % v
+
+
+try:
+    from xml.utils import iso8601
+    format_date = iso8601.tostring
+
+except:
+    format_date = iso8601_hack_tostring
+
 
 def get_formatted_date(entry):
     if not entry:
@@ -54,8 +95,7 @@ def get_formatted_date(entry):
     # if is_dst flag set, adjust for daylight savings time
     if time_tuple[8] == 1:
         tzoffset = time.altzone
-    return xml.utils.iso8601.tostring(time.mktime(time_tuple),tzoffset)    
-
+    return format_date(time.mktime(time_tuple), tzoffset)    
 
 def cb_head(args):
     entry = args["entry"]
@@ -71,11 +111,9 @@ def cb_head(args):
     entry["w3cdate"] = get_formatted_date(entrylist[0])
     return args
 
-
 def cb_story(args):
     entry = args['entry']
     entry["w3cdate"] = get_formatted_date(entry)
-
 
 def cb_foot(args):
     entry = args["entry"]
