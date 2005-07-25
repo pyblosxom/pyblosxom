30c3685e92e2b3b4d96b73bedaff6300396d6593
Will Guaraldi <willg@bluesock.org>
2005-07-25 22:03:24 +0000

    Fixed the output of the debug renderer.  It now sorts map keys and prints key names in blue.  Makes it much easier to read.

--- Pyblosxom/renderers/debug.py
+++ Pyblosxom/renderers/debug.py
@@ -6,6 +6,21 @@ from Pyblosxom.renderers.base import RendererBase
 from Pyblosxom import tools
 import types
 
+def E(s):
+    if not s:
+        return ""
+
+    if not type(s) == types.StringType:
+        s = repr(s)
+
+    return s.replace("&", "&amp;").replace(">", "&gt;").replace("<", "&lt;")
+
+def print_map(f, m):
+    keys = m.keys()
+    keys.sort()
+    for key in keys:
+        f("<font color=\"#0000ff\">%s</font> -&gt; %s\n" % (E(key), E(m[key])))
+
 class Renderer(RendererBase):
     def render(self, header = 1):
         pyhttp = self._request.getHttp()
@@ -13,48 +28,46 @@ class Renderer(RendererBase):
         data = self._request.getData()
         printout = self.write
 
-        self.addHeader('Content-type', 'text/plain')
+        self.addHeader('Content-type', 'text/html')
         self.showHeaders()
+        printout("<html>")
+        printout("<body>")
+        printout("<pre>")
         printout("Welcome to debug mode!\n")
         printout("You wanted the %(flavour)s flavour if I support flavours\n" % data)
 
         printout("------------------------------------------------------\n")
         printout("The HTTP Return codes are:\n")
         printout("------------------------------------------------------\n")
-        for key in self._header.keys():
-            printout("%s -> %s\n" % (key, self._header[key]))
+        print_map(printout, self._header)
 
         printout("------------------------------------------------------\n")
         printout("The OS environment contains:\n")
         printout("------------------------------------------------------\n")
         import os
-        for key in os.environ.keys():
-            printout("%s -> %s\n" % (key, os.environ[key]))
+        print_map(printout, os.environ)
 
         printout("------------------------------------------------------\n")
         printout("Request.getHttp() dict contains:\n")
         printout("------------------------------------------------------\n")
-        for key in pyhttp.keys():
-            printout("%s -> %s\n" % (key, pyhttp[key]))
+        print_map(printout, pyhttp)
 
         printout("------------------------------------------------------\n")
         printout("Request.getConfiguration() dict contains:\n")
         printout("------------------------------------------------------\n")
-        for key in config.keys():
-            printout("%s -> %s\n" % (key, config[key]))
+        print_map(printout, config)
 
         printout("------------------------------------------------------\n")
         printout("Request.getData() dict contains:\n")
         printout("------------------------------------------------------\n")
-        for key in data.keys():
-            printout("%s -> %s\n" % (key, data[key]))
+        print_map(printout, data)
 
         printout("------------------------------------------------------\n")
         printout("Entries to process:\n")
         printout("------------------------------------------------------\n")
         for content in self._content:
             if type(content) != types.StringType:
-                printout("%s\n" % content.get('filename', 'No such file\n'))
+                printout("%s\n" % E(content.get('filename', 'No such file\n')))
 
         printout("------------------------------------------------------\n")
         printout("Entries processed:\n")
@@ -62,12 +75,9 @@ class Renderer(RendererBase):
         for content in self._content:
             if type(content) != types.StringType:
                 printout("------------------------------------------------------\n")
-                printout("Items for %s:\n" % content.get('filename', 'No such file\n'))
+                printout("Items for %s:\n" % E(content.get('filename', 'No such file\n')))
                 printout("------------------------------------------------------\n")
-                for item in content.keys():
-                    printout("%s -> %s\n" % (item, content[item]))
-
-                #printout("%s\n" % content.get('filename', 'No such file\n'))
+                print_map(printout, content)
 
         printout("------------------------------------------------------\n")
         printout("Cached Titles:\n")
@@ -78,7 +88,7 @@ class Renderer(RendererBase):
                 filename = content['filename']
             
                 if cache.has_key(filename):
-                    printout("%s\n" % cache[filename]['title'])
+                    printout("%s\n" % E(cache[filename]['title']))
                 cache.close()
 
         printout("------------------------------------------------------\n")
@@ -88,10 +98,12 @@ class Renderer(RendererBase):
             if type(content) != types.StringType:
                 filename = content['filename']
                 if cache.has_key(filename):
-                    printout("%s\n" % cache[filename]['title'])
+                    printout("%s\n" % E(cache[filename]['title']))
                     printout("==================================================\n")
-                    printout("%s\n" % cache[filename]['body'])
+                    printout("%s\n" % E(cache[filename]['body']))
                 else:
-                    printout("Contents of %s is not cached\n" % filename)
+                    printout("Contents of %s is not cached\n" % E(filename))
                 cache.close()
                 printout("------------------------------------------------------\n")
+        printout("</body>")
+        printout("</html>")
