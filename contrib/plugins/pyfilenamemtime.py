8c62f40997688d1edb5733d959beb6b764bbd0b6
Will Guaraldi <willg@bluesock.org>
2005-08-01 18:48:38 +0000

    Merged in changes from PYBLOSXOM_1_2. Added copyright and license information.

--- contrib/plugins/pyfilenamemtime.py
+++ contrib/plugins/pyfilenamemtime.py
@@ -4,6 +4,28 @@ If a filename contains a timestamp in the form of YYYY-MM-DD-hh-mm,
 change the mtime to be the timestamp instead of the one kept by the
 filesystem.  For example, a valid filename would be
 foo-2002-04-01-00-00.txt for April fools day on the year 2002.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Copyright 2004, 2005 Tim Roberts
 """
 import os, re, time
 
