e4f1bd94a0a1f4e5a864d1f2e83b3f0277c70e47
Will Guaraldi <willg@bluesock.org>
2005-11-15 19:29:01 +0000

    Changed "date", "w3cdate", and "rfc822date" for head and foot templates to "latest_date", "latest_w3cdate", and "latest_rfc822date".  The names aren't wildly great, but this avoids clobbering of data in the entry object which was causing issues with story templates.

--- Pyblosxom/pyblosxom.py
+++ Pyblosxom/pyblosxom.py
@@ -776,9 +776,9 @@ def blosxom_handler(request):
     mtime_tuple = time.localtime(mtime)
     mtime_gmtuple = time.gmtime(mtime)
 
-    data["date"] = time.strftime('%a, %d %b %Y', mtime_tuple)
-    data["w3cdate"] = time.strftime('%Y-%m-%dT%H:%M:%SZ', mtime_gmtuple)
-    data['rfc822date'] = time.strftime('%a, %d %b %Y %H:%M GMT', mtime_gmtuple)
+    data["latest_date"] = time.strftime('%a, %d %b %Y', mtime_tuple)
+    data["latest_w3cdate"] = time.strftime('%Y-%m-%dT%H:%M:%SZ', mtime_gmtuple)
+    data['latest_rfc822date'] = time.strftime('%a, %d %b %Y %H:%M GMT', mtime_gmtuple)
 
     # we pass the request with the entry_list through the prepare callback
     # giving everyone a chance to transform the data.  the request is
