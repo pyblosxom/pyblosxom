83c1e98b3f2199ee309ac4d711a62cf23ba408fd
Will Guaraldi <willg@bluesock.org>
2007-03-02 16:50:08 +0000

    Added a _path_pyblosxom.py which finds the path to pyblosxom and adds it to the system path.  Makes it easier to run all the unit tests.

---
+++ tests/unit/_path_pyblosxom.py
@@ -0,0 +1,5 @@
+import sys, os
+testdir = os.path.dirname(__file__)
+
+sys.path.insert(0, os.path.join(testdir, '../../'))
+
--- tests/unit/test_tools.py
+++ tests/unit/test_tools.py
@@ -1,3 +1,5 @@
+import _path_pyblosxom
+
 import string
 from Pyblosxom import tools
 
