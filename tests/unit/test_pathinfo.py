2bb8d4bab6cae1f7389df9cdfa502aab7cd2c5d3
Will Guaraldi <willg@bluesock.org>
2008-01-28 00:54:32 +0000

    Added a few more pathinfo tests.  Also rewrote some of the test setup/teardown code.

--- tests/unit/test_pathinfo.py
+++ tests/unit/test_pathinfo.py
@@ -2,6 +2,8 @@ import _path_pyblosxom
 
 import os
 import os.path
+import shutil
+import tempfile
 import StringIO
 
 from Pyblosxom.pyblosxom import Request, blosxom_process_path_info
@@ -12,11 +14,14 @@ class Testpathinfo:
 
     This tests default parsing of the path.
     """
+
+    _tempdir = tempfile.mkdtemp()
+
     def _setup(self, files):
         # sort so that we're building the directories in order
         files.sort()
 
-        os.makedirs(os.path.join(os.getcwd(), "entries"))
+        os.makedirs(os.path.join(Testpathinfo._tempdir, "entries"))
 
         for fn in files:
             d, f = os.path.split(fn)
@@ -31,31 +36,11 @@ class Testpathinfo:
                 f.write("test file: %s\n" % fn)
                 f.close()
             
-    def _teardown(self, files):
-        # sort and reverse so that we're tearing down directories in the
-        # reverse order that we built them.
-        files.sort()
-        files.reverse()
-
-        for fn in files:
-            d, f = os.path.split(fn)
-
-            if f:
-                try:
-                    os.remove(fn)
-                except OSError, e:
-                    pass
-             
-            try:
-                os.rmdir(d)
-
-            except OSError, e:
-                pass
-
-        os.rmdir(os.path.join(os.getcwd(), "entries"))
+    def _teardown(self):
+        shutil.rmtree(Testpathinfo._tempdir)
 
     def _buildfileset(self, filelist):
-        return [ os.path.join(os.getcwd(), "entries/%s" % fn) for fn in filelist ]
+        return [ os.path.join(Testpathinfo._tempdir, "entries/%s" % fn) for fn in filelist ]
 
     def _build_request(self, cfg={}, http={}, data={}, inputstream=""):
         """
@@ -75,7 +60,7 @@ class Testpathinfo:
         - req.pyhttp["CONTENT_LENGTH"]    - integer
         """
         _config = { "default_flavour": "html", 
-                    "datadir": os.path.join(os.getcwd(), "entries"),
+                    "datadir": os.path.join(Testpathinfo._tempdir, "entries"),
                     "blog_title": "Joe's blog",
                     "base_url": "http://www.example.com/" }
         _config.update(cfg)
@@ -101,7 +86,7 @@ class Testpathinfo:
                 assert os.path.isfile( mem )
 
         finally:
-            self._teardown(fileset1)
+            self._teardown()
 
         for mem in fileset1:
             assert not os.path.isfile( mem )
@@ -113,6 +98,7 @@ class Testpathinfo:
     def _basic_test(self, pathinfo, expected):
         req = self._build_request(http={ "PATH_INFO": pathinfo })
         blosxom_process_path_info(args={"request": req})
+        print repr(expected), repr(req.data)
         self.cmpdict( expected, req.data )
  
     def test_root(self):
@@ -121,19 +107,22 @@ class Testpathinfo:
         self._setup(entries)
         try:
             # /
-            self._basic_test( "/", { "bl_type": "dir",
-                                     "pi_yr": "",
-                                     "pi_mo": "",
-                                     "pi_da": "",
-                                     "flavour": "html" } )
+            self._basic_test( "/",
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
             # /index
-            self._basic_test( "/index", { "bl_type": "dir",
-                                          "pi_yr": "",
-                                          "pi_mo": "",
-                                          "pi_da": "",
-                                          "flavour": "html" } )
+            self._basic_test( "/index", 
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /index.xml
+            self._basic_test( "/index.xml", 
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "xml" } )
         finally:
-            self._teardown(entries)
+            self._teardown()
 
     def test_files(self):
         entries = self._buildfileset( [ "file1.txt", "cata/file2.txt", "catb/file3.txt" ] )
@@ -141,35 +130,113 @@ class Testpathinfo:
         self._setup(entries)
         try:
             # /file1
-            self._basic_test( "/file1", { "bl_type": "file",
-                                          "pi_yr": "",
-                                          "pi_mo": "",
-                                          "pi_da": "",
-                                          "flavour": "html" } )
+            self._basic_test( "/file1", 
+                              { "bl_type": "file",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
             # /cata/file2
-            self._basic_test( "/cata/file2", { "bl_type": "file",
-                                               "pi_yr": "",
-                                               "pi_mo": "",
-                                               "pi_da": "",
-                                               "flavour": "html" } )
+            self._basic_test( "/cata/file2", 
+                              { "bl_type": "file",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
         finally:
-            self._teardown(entries)
+            self._teardown()
+
+    def test_categories(self):
+        entries = self._buildfileset( [ "cata/entry1.txt", "cata/suba/entry1.txt", "catb/entry1.txt" ])
+
+        self._setup(entries)
+        try:
+            # /cata
+            self._basic_test( "/cata", 
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /cata/
+            self._basic_test( "/cata/", 
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /cata/suba
+            self._basic_test( "/cata/suba", 
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /cata/suba
+            self._basic_test( "/cata/suba/entry1.html", 
+                              { "bl_type": "file",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+        finally:
+            self._teardown()
 
     def test_dates(self):
         tools.initialize( {} )
 
-        self._basic_test( "/2002", { "bl_type": "dir",
-                                      "pi_yr": "2002",
-                                      "pi_mo": "",
-                                      "pi_da": "",
-                                      "flavour": "html" } )
-        self._basic_test( "/2002/02", { "bl_type": "dir",
-                                        "pi_yr": "2002",
-                                        "pi_mo": "02",
-                                        "pi_da": "",
-                                        "flavour": "html" } )
-        self._basic_test( "/2002/02/04", { "bl_type": "dir",
-                                           "pi_yr": "2002",
-                                           "pi_mo": "02",
-                                           "pi_da": "04",
-                                           "flavour": "html" } )
+        self._basic_test( "/2002",
+                          { "bl_type": "dir",
+                            "pi_yr": "2002", "pi_mo": "", "pi_da": "",
+                            "flavour": "html" } )
+        self._basic_test( "/2002/02",
+                          { "bl_type": "dir",
+                            "pi_yr": "2002", "pi_mo": "02", "pi_da": "",
+                            "flavour": "html" } )
+        self._basic_test( "/2002/02/04", 
+                          { "bl_type": "dir",
+                            "pi_yr": "2002", "pi_mo": "02", "pi_da": "04",
+                            "flavour": "html" } )
+
+    def test_categories_and_dates(self):
+        tools.initialize( {} )
+        entries = self._buildfileset( [ "cata/entry1.txt", "cata/suba/entry1.txt", "catb/entry1.txt" ])
+
+        self._setup(entries)
+        try:
+            # /2006/cata/
+            self._basic_test( "/2006/cata/", 
+                              { "bl_type": "dir",
+                                "pi_yr": "2006", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /2006/04/cata/
+            self._basic_test( "/2006/04/cata/", 
+                              { "bl_type": "dir",
+                                "pi_yr": "2006", "pi_mo": "04", "pi_da": "",
+                                "flavour": "html" } )
+            # /2006/04/02/cata/
+            self._basic_test( "/2006/04/02/cata/", 
+                              { "bl_type": "dir",
+                                "pi_yr": "2006", "pi_mo": "04", "pi_da": "02",
+                                "flavour": "html" } )
+            # /2006/04/02/cata/suba/
+            self._basic_test( "/2006/04/02/cata/suba/", 
+                              { "bl_type": "dir",
+                                "pi_yr": "2006", "pi_mo": "04", "pi_da": "02",
+                                "flavour": "html" } )
+
+        finally:
+            self._teardown()
+
+    def test_date_categories(self):
+        tools.initialize( {} )
+        entries = self._buildfileset( [ "2007/entry1.txt", "2007/05/entry3.txt", "cata/entry2.txt" ] )
+
+        self._setup(entries)
+        try:
+            # /2007/              2007 here is a category
+            self._basic_test( "/2007/",
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /2007/05            2007/05 here is a category
+            self._basic_test( "/2007/05",
+                              { "bl_type": "dir",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+            # /2007/05/entry3     2007/05/entry3 is a file
+            self._basic_test( "/2007/05/entry3.html",
+                              { "bl_type": "file",
+                                "pi_yr": "", "pi_mo": "", "pi_da": "",
+                                "flavour": "html" } )
+
+        finally:
+            self._teardown()
