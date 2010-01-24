0cc16153adefb86285d297c3946fb7b16e0bf5f5
Will Guaraldi <willg@bluesock.org>
2010-01-24 16:31:38 +0000

    Updated test_check_javascript plugin.

--- plugins/tests/__init__.py
+++ plugins/tests/__init__.py
@@ -10,5 +10,5 @@
 import unittest
 
 # from plugins.tests.comments.test_akismetcomments import *
-# from plugins.tests.comments.test_check_javascript import *
+from plugins.tests.comments.test_check_javascript import *
 from plugins.tests.comments.test_comments import *
--- plugins/tests/comments/test_check_javascript.py
+++ plugins/tests/comments/test_check_javascript.py
@@ -5,10 +5,8 @@ Tests for the check_javascript plugin.
 __author__ = 'Ryan Barrett <pyblosxom@ryanb.org>'
 __url__ = 'http://pyblosxom.sourceforge.net/wiki/index.php/Framework_for_testing_plugins'
 
-from nose.tools import eq_
 from plugins.tests.test_base import PluginTest
 from plugins.comments.plugins import check_javascript
-import cgi, cPickle, os, StringIO, sys, urllib
 
 class TestCheckJavascript(PluginTest):
     """Test class for the check_javascript plugin.
@@ -21,12 +19,12 @@ class TestCheckJavascript(PluginTest):
         """check_javascript should check the secretToken query argument."""
         # no secretToken
         assert 'secretToken' not in self.http
-        eq_(True, check_javascript.cb_comment_reject(self.args))
+        self.assertEquals(True, check_javascript.cb_comment_reject(self.args))
 
         # bad secretToken
         self.set_form_data({'secretToken': 'not the title'})
-        eq_(True, check_javascript.cb_comment_reject(self.args))
+        self.assertEquals(True, check_javascript.cb_comment_reject(self.args))
 
         # good secretToken
         self.set_form_data({'secretToken': 'test title'})
-        eq_(False, check_javascript.cb_comment_reject(self.args))
+        self.assertEquals(False, check_javascript.cb_comment_reject(self.args))
