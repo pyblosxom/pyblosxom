e6b338830aabbc8b010c5db4b0ab3e4ff1027cf5
Will Kahn-Greene <willg@bluesock.org>
2010-04-03 10:54:59 -0400

    Removed long description from setup.py--it pulls the README now.

--- setup.py
+++ setup.py
@@ -11,39 +11,11 @@ except ImportError:
 
 version = "1.5-rc1"
 
-DESC = """PyBlosxom
-=========
-
-Summary
--------
-
-PyBlosxom is a file-based weblog engine originally inspired by
-Blosxom.  It supports user-created plugins to augment and extend the
-default behavior.  It supports Paste, WSGI, and CGI and can run in a
-variety of environments.
-
-
-Download and installation
--------------------------
-
-To download and install PyBlosxom you can get the .tar.gz file at::
-
-   http://pyblosxom.sourceforge.net/
-
-Or you can use pip (http://pypi.python.org/pypi/pip)::
-
-   pip install pyblosxom
-
-If you have easy_install, but don't have pip, you can do::
-
-   easy_install pyblosxom
-"""
-
 setup(
     name="pyblosxom",
     version=version,
     description="PyBlosxom is a file-based weblog engine.",
-    long_description=DESC,
+    long_description=open('README').read(),
     license='MIT',
     author="Will Kahn-Greene, et al",
     author_email="willg@bluesock.org",
