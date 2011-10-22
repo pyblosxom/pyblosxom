4dd729d560642a0997ec6cbbba94e52fba0125d7
Will Kahn-Greene <willg@bluesock.org>
2011-10-22 13:00:05 -0400

    pep8 and updates to entrytitle

--- Pyblosxom/plugins/entrytitle.py
+++ Pyblosxom/plugins/entrytitle.py
@@ -1,7 +1,7 @@
 #######################################################################
 # This file is part of PyBlosxom.
 #
-# Copyright (C) 2010 by the PyBlosxom team.  See AUTHORS.
+# Copyright (C) 2010, 2011 by the PyBlosxom team.  See AUTHORS.
 #
 # PyBlosxom is distributed under the MIT license.  See the file
 # LICENSE for distribution details.
@@ -11,27 +11,27 @@
 Summary
 =======
 
-If PyBlosxom is rendering a single entry (i.e. entry_list has 1 item
-in it), then this populates the ``entry_title`` variable for the
-header template.
+If PyBlosxom is rendering a single entry (i.e. entry_list has 1 item in it),
+then this populates the ``entry_title`` variable for the header template.
+
 
 Usage
 =====
 
-To use, add the ``$entry_title`` variable to your header template in
-the <title> area.
+To use, add the ``$entry_title`` variable to your header template in the
+``<title>`` area.
 
 Example::
 
     <title>$(blog_title)$(entry_title)</title>
 
-The default $(entry_title) starts with a :: and ends with the title of
-the entry.  For example::
+The default ``$(entry_title)`` starts with a ``::`` and ends with the title
+of the entry.  For example::
 
     :: Guess what happened today
 
 You can set the entry title template in the configuration properties
-with the ``entry_title_template`` variable.
+with the ``entry_title_template`` variable::
 
     config["entry_title_template"] = ":: %(title)s"
 
@@ -41,7 +41,7 @@ the entry title.
 
 __author__ = "Will Kahn-Greene"
 __email__ = "willg at bluesock dot org"
-__version__ = "2010-04-14"
+__version__ = "2011-10-22"
 __url__ = "http://pyblosxom.bluesock.org/"
 __description__ = "Populates $(entry_title) for head template."
 __category__ = "date"
@@ -50,17 +50,20 @@ __registrytags__ = "1.4, 1.5, core"
 
 
 def verify_installation(request):
+    # This needs no verification.
     return 1
+
 
 def cb_head(args):
     req = args["request"]
     entry = args["entry"]
-    
+
     data = req.get_data()
     entry_list = data.get("entry_list", [])
     if len(entry_list) == 1:
         config = req.get_configuration()
         tmpl = config.get("entry_title_template", ":: %(title)s")
-        entry["entry_title"] = tmpl % {"title": entry_list[0].get("title", "No title")}
+        entry["entry_title"] = (tmpl %
+            {"title": entry_list[0].get("title", "No title")})
 
     return args
