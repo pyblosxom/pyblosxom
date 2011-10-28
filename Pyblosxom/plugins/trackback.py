cecfead06e84fff5c65b98769239e00f891849af
Will Kahn-Greene <willg@bluesock.org>
2011-10-28 17:48:08 -0400

    Move plugins, pep8 and docs fixes

---
+++ Pyblosxom/plugins/trackback.py
@@ -0,0 +1,175 @@
+#######################################################################
+# This file is part of PyBlosxom.
+#
+# Copyright (c) 2003-2005 Ted Leung
+#
+# PyBlosxom is distributed under the MIT license.  See the file
+# LICENSE for distribution details.
+#######################################################################
+
+"""
+Summary
+=======
+
+This plugin allows pyblosxom to process trackback
+http://www.sixapart.com/pronet/docs/trackback_spec pings.
+
+
+Install
+=======
+
+Requires the ``comments`` plugin.  Though you don't need to have
+comments enabled on your blog in order for trackbacks to work.
+
+1. Add ``Pyblosxom.plugins.trackback`` to the ``load_plugins`` list
+   in your ``config.py`` file.
+
+2. Add this to your ``config.py`` file::
+
+       py['trackback_urltrigger'] = "/trackback"
+
+   These web forms are useful for testing.  You can use them to send
+   trackback pings with arbitrary content to the URL of your choice:
+
+   * http://kalsey.com/tools/trackback/
+   * http://www.reedmaniac.com/scripts/trackback_form.php
+
+3. Now you need to advertise the trackback ping link.  Add this to your
+   ``story`` template::
+
+       <a href="$(base_url)/trackback/$(file_path)" title="Trackback">TB</a>
+
+4. You can supply an embedded RDF description of the trackback ping, too.
+   Add this to your ``story`` or ``comment-story`` template::
+
+       <!--
+       <rdf:RDF
+       xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+       xmlns:dc="http://purl.org/dc/elements/1.1/"
+       xmlns:trackback="http://madskills.com/public/xml/rss/module/trackback/">
+       <rdf:Description
+            about="$(base_url)/$(file_path)"
+            dc:title="$(title)"
+            dc:identifier="$(base_url)/$(file_path)"
+            trackback:ping="$(base_url)/trackback/$(file_path)"
+       />
+       </rdf:RDF>
+       -->
+
+"""
+
+__author__ = "Ted Leung"
+__email__ = ""
+__version__ = ""
+__url__ = "http://pyblosxom.bluesock.org/"
+__description__ = "Trackback support."
+__category__ = "comments"
+__license__ = "MIT"
+__registrytags__ = "1.4, core"
+
+
+from Pyblosxom import tools
+from Pyblosxom.tools import pwrap_error
+
+
+tb_good_response = """<?xml version="1.0" encoding="iso-8859-1"?>
+<response>
+<error>0</error>
+</response>"""
+
+
+tb_bad_response = """<?xml version="1.0" encoding="iso-8859-1"?>
+<response>
+<error>1</error>
+<message>%s</message>
+</response>"""
+
+
+def verify_installation(request):
+    config = request.get_configuration()
+    retval = 1
+
+    # all config properties are optional
+    if not 'trackback_urltrigger' in config:
+        pwrap_error("missing optional property: 'trackback_urltrigger'")
+
+    return retval
+
+
+def cb_handle(args):
+    request = args['request']
+    pyhttp = request.get_http()
+    config = request.get_configuration()
+
+    urltrigger = config.get('trackback_urltrigger', '/trackback')
+
+    logger = tools.get_logger()
+
+    path_info = pyhttp['PATH_INFO']
+    if path_info.startswith(urltrigger):
+        response = request.get_response()
+        response.add_header("Content-type", "text/xml")
+
+        form = request.get_form()
+
+        message = ("A trackback must have at least a URL field (see "
+                   "http://www.sixapart.com/pronet/docs/trackback_spec)")
+
+        if "url" in form:
+            from comments import decode_form
+            encoding = config.get('blog_encoding', 'iso-8859-1')
+            decode_form(form, encoding)
+            import time
+            cdict = {'title': form.getvalue('title', ''),
+                     'author': form.getvalue('blog_name', ''),
+                     'pubDate': str(time.time()),
+                     'link': form['url'].value,
+                     'source': form.getvalue('blog_name', ''),
+                     'description': form.getvalue('excerpt', ''),
+                     'ipaddress': pyhttp.get('REMOTE_ADDR', ''),
+                     'type': 'trackback'
+                     }
+            argdict = {"request": request, "comment": cdict}
+            reject = tools.run_callback("trackback_reject",
+                                        argdict,
+                                        donefunc=lambda x: x != 0)
+            if isinstance(reject, (tuple, list)) and len(reject) == 2:
+                reject_code, reject_message = reject
+            else:
+                reject_code, reject_message = reject, "Trackback rejected."
+
+            if reject_code == 1:
+                print >> response, tb_bad_response % reject_message
+                return 1
+
+            from Pyblosxom.entries.fileentry import FileEntry
+
+            datadir = config['datadir']
+
+            from comments import writeComment
+            try:
+                import os
+                pi = path_info.replace(urltrigger, '')
+                path = os.path.join(datadir, pi[1:])
+                data = request.get_data()
+                ext = tools.what_ext(data['extensions'].keys(), path)
+                entry = FileEntry(request, '%s.%s' % (path, ext), datadir)
+                data = {}
+                data['entry_list'] = [entry]
+                # Format Author
+                cdict['author'] = (
+                    'Trackback from %s' % form.getvalue('blog_name', ''))
+                writeComment(request, config, data, cdict, encoding)
+                print >> response, tb_good_response
+            except OSError:
+                message = 'URI ' + path_info + " doesn't exist"
+                logger.error(message)
+                print >> response, tb_bad_response % message
+
+        else:
+            logger.error(message)
+            print >> response, tb_bad_response % message
+
+        # no further handling is needed
+        return 1
+    return 0
---
+++ Pyblosxom/plugins/xmlrpc_pingback.py
@@ -0,0 +1,210 @@
+#######################################################################
+# This file is part of PyBlosxom.
+#
+# Copyright (c) 2003-2006 Ted Leung, Ryan Barrett
+#
+# PyBlosxom is distributed under the MIT license.  See the file
+# LICENSE for distribution details.
+#######################################################################
+
+"""
+Summary
+=======
+
+This module contains an XML-RPC extension to support pingback
+http://www.hixie.ch/specs/pingback/pingback pings.
+
+
+Install
+=======
+
+Requires the ``comments`` plugin, but you don't have to enable
+comments on your blog for pingbacks to work.
+
+1. Add ``Pyblosxom.plugins.xmlrpc_pingback`` to the ``load_plugins``
+   list of your ``config.py`` file
+
+2. Set the ``xmlrpc_trigger`` variable in your ``config.py`` file to a
+   trigger for this plugin.  For example::
+
+      py["xmlrpc_trigger"] = "RPC"
+
+3. Add to the ``<head>`` section of your ``head`` template::
+
+      <link rel="pingback" href="$(base_url)/RPC" />
+
+
+This test blog, maintained by Ian Hickson, is useful for testing. You
+can post to it, linking to a post on your site, and it will send a
+pingback.
+
+* http://www.dummy-blog.org/
+
+"""
+
+__author__ = "Ted Leung, Ryan Barrett"
+__email__ = ""
+__version__ = "2011-10-28"
+__url__ = "http://pyblosxom.bluesock.org/"
+__description__ = "XMLRPC pingback support."
+__category__ = "comments"
+__license__ = "MIT"
+__registrytags__ = "1.4, core"
+
+
+from Pyblosxom import tools
+from xmlrpclib import Fault
+
+import re
+import sgmllib
+import time
+import urllib
+import urlparse
+
+
+def verify_installation(request):
+    # no config parameters
+    return 1
+
+
+class parser(sgmllib.SGMLParser):
+    """ Shamelessly grabbed from Sam Ruby
+    from http://www.intertwingly.net/code/mombo/pingback.py
+    """
+    intitle = 0
+    title = ""
+    hrefs = []
+
+    def do_a(self, attrs):
+        attrs = dict(attrs)
+        if 'href' in attrs:
+            self.hrefs.append(attrs['href'])
+
+    def do_title(self, attrs):
+        if self.title == "":
+            self.intitle = 1
+
+    def unknown_starttag(self, tag, attrs):
+        self.intitle = 0
+
+    def unknown_endtag(self, tag):
+        self.intitle = 0
+
+    def handle_charref(self, ref):
+        if self.intitle:
+            self.title = self.title + ("&#%s;" % ref)
+
+    def handle_data(self, text):
+        if self.intitle:
+            self.title = self.title + text
+
+
+def fileFor(req, uri):
+    config = req.get_configuration()
+    urldata = urlparse.urlsplit(uri)
+
+    # Reconstruct uri to something sane
+    uri = "%s://%s%s" % (urldata[0], urldata[1], urldata[2])
+    fragment = urldata[4]
+
+    # We get our path here
+    path = uri.replace(config['base_url'], '')
+    req.add_http({'PATH_INFO': path, "form": {}})
+    from Pyblosxom.pyblosxom import blosxom_process_path_info
+    blosxom_process_path_info({'request': req})
+
+    args = {'request': req}
+    from Pyblosxom.pyblosxom import blosxom_file_list_handler
+    es = blosxom_file_list_handler(args)
+
+    # We're almost there
+    if len(es) == 1 and path.find(es[0]['file_path']) >= 0:
+        return es[0]
+
+    # Could be a fragment link
+    for i in es:
+        if i['fn'] == fragment:
+            return i
+
+    # Point of no return
+    if len(es) >= 1:
+        raise Fault(0x0021, "%s cannot be used as a target" % uri)
+    else:
+        raise Fault(0x0020, "%s does not exist")
+
+
+def pingback(request, source, target):
+    logger = tools.get_logger()
+    logger.info("pingback started")
+    source_file = urllib.urlopen(source.split('#')[0])
+    if source_file.headers.get('error', '') == '404':
+        raise Fault(0x0010, "Target %s not exists" % target)
+    source_page = parser()
+    source_page.feed(source_file.read())
+    source_file.close()
+
+    if source_page.title == "":
+        source_page.title = source
+
+    if not target in source_page.hrefs:
+        raise Fault(0x0011, "%s does not point to %s" % (source, target))
+
+    target_entry = fileFor(request, target)
+
+    body = ''
+    try:
+        from rssfinder import getFeeds
+        from rssparser import parse
+
+        baseurl = source.split("#")[0]
+        for feed in getFeeds(baseurl):
+            for item in parse(feed)['items']:
+                if item['link'] == source:
+                    if 'title' in item:
+                        source_page.title = item['title']
+                    if 'content_encoded' in item:
+                        body = item['content_encoded'].strip()
+                    if 'description' in item:
+                        body = item['description'].strip() or body
+                    body = re.compile('<.*?>', re.S).sub('', body)
+                    body = re.sub('\s+', ' ', body)
+                    body = body[:body.rfind(' ', 0, 250)][:250] + " ...<br />"
+    except:
+        pass
+
+    cmt = {'title': source_page.title,
+           'author': 'Pingback from %s' % source_page.title,
+           'pubDate': str(time.time()),
+           'link': source,
+           'source': '',
+           'description': body}
+
+    # run anti-spam plugins
+    argdict = {"request": request, "comment": cmt}
+    reject = tools.run_callback("trackback_reject",
+                                argdict,
+                                donefunc=lambda x: x != 0)
+    if isinstance(reject, (tuple, list)) and len(reject) == 2:
+        reject_code, reject_message = reject
+    else:
+        reject_code, reject_message = reject, "Pingback rejected."
+    if reject_code == 1:
+        raise Fault(0x0031, reject_message)
+
+    from comments import writeComment
+    config = request.get_configuration()
+    data = request.get_data()
+    data['entry_list'] = [target_entry]
+
+    # TODO: Check if comment from the URL exists
+    writeComment(request, config, data, cmt, config['blog_encoding'])
+
+    return "success pinging %s from %s\n" % (target, source)
+
+
+def cb_xmlrpc_register(args):
+    """
+    Register as a pyblosxom XML-RPC plugin
+    """
+    args['methods'].update({'pingback.ping': pingback})
+    return args
